1aa513e8daeab564740956190b1cb3507d5db97b
wangshaoshuai <1404839247@qq.com>
2019-11-24 20:30:20 +0800

    Webpack打包less

--- Vue学习/04-day/06-WebPack的起步/src/main.js
+++ Vue学习/04-day/06-WebPack的起步/src/main.js
@@ -12,3 +12,17 @@ console.log(height);
 
 //3.依赖css文件
 require('./css/normal.css')
+
+// 4.依赖less文件 npm install --save-dev less-loader less
+require('./css/special.less')
+
+// 5. 引用图片的 npm install --save-dev url-loader
+// 6. ES6的语法转成ES5的语法，需要使用babel
+// 7. 使用Vue进行开发
+import Vue from 'vue'
+const app = new Vue({
+  el: '#app',
+  data: {
+    message: 'Hello WebPack'
+  }
+})
--- Vue学习/04-day/06-WebPack的起步/webpack.config.js
+++ Vue学习/04-day/06-WebPack的起步/webpack.config.js
@@ -8,18 +8,57 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     // 文件名
     filename: 'handel.js',
+    // 涉及到路径的前面都将加上dist/
+    publicPath: 'dist/'
   },
   module: {
     rules: [
-      // css-loader只负责将文件进行加载
-      //  style-loader将样式绘制到DOM上
+      // 负责对css文件进行处理
       { 
         test: /\.css$/, 
         use: [
+          //  style-loader将样式绘制到DOM上
           { loader: "style-loader" },
+          // css-loader只负责将文件进行加载
           { loader: "css-loader" }
         ]
-      }
+      },
+      // webpack 处理less文件的配置
+      {
+        test: /\.less$/,
+        use: [{
+            loader: "style-loader" // creates style nodes from JS strings
+        }, {
+            loader: "css-loader" // translates CSS into CommonJS
+        }, {
+            loader: "less-loader" // compiles Less to CSS
+        }]
+      },
+      // 当加载的图片大于limit时
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {}
+          }
+        ]
+      },
+      // 图片的处理
+      // {
+      //   test: /\.(png|jpg|gif)$/,
+      //   use: [
+      //     {
+      //       loader: 'url-loader',
+      //       options: {
+      //         // 当加载的图片小于limit时，会将图片编译成base64字符串形式/*
+      //         limit: 8192000000,
+      //         // 打包后的文件名+哈希值8位+扩展名
+      //         name: 'img/[name].[hash:8].[ext]'
+      //       }
+      //     }
+      //   ]
+      // },
     ]
   }
 }
