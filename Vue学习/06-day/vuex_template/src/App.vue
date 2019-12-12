406e60ad219c8dd68076668b9624db4b49e4bd5d
wssGod <1404839247@qq.com>
2019-12-12 18:44:24 +0800

    Vuex的学习

--- Vue学习/06-day/vuex_template/src/App.vue
+++ Vue学习/06-day/vuex_template/src/App.vue
@@ -7,12 +7,17 @@
     <button @click="addCount(5)">+5</button>
     <button @click="addCount(10)">+10</button>
     <hello-world></hello-world>
-    <h2>---------App内容：getters相关信息---------</h2>
+    <h2>---------App内容：相关信息---------</h2>
     <h2>{{ $store.getters.powerCount }}</h2>
+    <h2>---------App内容：modules里面的内容---------</h2>
+    <h2>{{ $store.state.a.name }}</h2>
   </div>
 </template>
 <script>
 import HelloWorld from '@/components/HelloWorld'
+import {
+  INCREMENT
+} from '@/store/mutation-types'
 export default {
   name: 'App',
   components: {
@@ -26,13 +31,23 @@ export default {
   },
   methods: {
     addClick() {
-      this.$store.commit('increment')
+      this.$store.commit(INCREMENT)
     },
     deClick() {
       this.$store.commit('decrement')
     },
     addCount(count) {
-      this.$store.commit('addCount', count)
+      // this.$store.commit('addCount', count)
+      // this.$store.dispatch('aAddCount', {
+      //   count: count,
+      //   message: '我是携带的信息',
+      //   success: () => {
+      //     console.log('里面已经完成')
+      //   }
+      // })
+      this.$store.dispatch('bAddCount', count).then((data) => {
+        console.log(data)
+      })
     }
   }
 }
--- Vue学习/06-day/vuex_template/src/store/index.js
+++ Vue学习/06-day/vuex_template/src/store/index.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import {
+  INCREMENT
+} from './mutation-types'
 // 安装插件
 Vue.use(Vuex)
-
+const modulesA = {
+  state: {
+    name: 'WSS'
+  }
+}
 // 2.创建对象
 const store = new Vuex.Store({
   // 存储状态的
@@ -11,7 +18,7 @@ const store = new Vuex.Store({
   },
   // 方法
   mutations: {
-    increment(state) {
+    [INCREMENT](state) {
       state.counter++
     },
     decrement(state) {
@@ -23,7 +30,23 @@ const store = new Vuex.Store({
   },
   // 进行异步操作（网络请求）
   actions: {
-
+    // context 上下文
+    aAddCount(context,payload) {
+      setTimeout(() => {
+        context.commit('addCount', payload.count)
+        console.log(payload.message)
+        payload.success()
+      }, 1000)
+    },
+    bAddCount(context,payload) {
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          context.commit('addCount', payload)
+          console.log('携带的参数',payload)
+          resolve('成功回调')
+        },1000)
+      })
+    }
   },
   getters: {
     powerCount(state) {
@@ -31,9 +54,8 @@ const store = new Vuex.Store({
     }
   },
   modules: {
-
+    a: modulesA
   }
 })
-
 // 3.导出
 export default store
